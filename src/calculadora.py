575786b3bf4f3801e5f979b7819df62d1a3b1ec6
Jhon <Doe@jhon.com>
2024-08-21 18:46:34 -0500

    Solución
    Se agrega operación potencia y módulo

--- src/calculadora.py
+++ src/calculadora.py
@@ -13,9 +13,12 @@ def calcular(numero1, numero2, operacion):
         case '/':
             # Operación división
             respuesta = numero1 / numero2
-
-        # TODO: DEBES COLOCAR TU CÓDIGO AQUÍ# TODO: DEBES CREAR AQUÍ LOS CASES PARA LA OPERACIÓN DE LA POTENCIA Y EL MÓDULO, COMPROBANDO LOS SÍMBOLOS ^ Y %.
-
+        case '^':
+            # Operación potencia
+            respuesta = numero1 ** numero2
+        case '%':
+            # Operación Módulo
+            respuesta = numero1 % numero2
         case _:
             raise ValueError("Operación inválida.")
 
